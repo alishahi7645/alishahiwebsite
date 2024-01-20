79f598c652e3dc1ad7b2d706971ed9b524c23e49
alishahi <shahy8227@gmail.com>
2024-01-20 10:36:17 -0800

    start code portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './Components/about/About';
 import Home from './Components/home/Home';
 import Navbar from './Components/navbar/Navbar';
+import Portfolio from './Components/portfolio/Portfolio';
 import Services from './Components/servics/Services';
 import './index.css';
 import {BrowserRouter , Route , Routes} from 'react-router-dom'
@@ -11,6 +12,7 @@ function App() {
       <Home/>
       <About/>
       <Services/>
+      <Portfolio/>
     </BrowserRouter>
   );
 }
