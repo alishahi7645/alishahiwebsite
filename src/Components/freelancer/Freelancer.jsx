02a8bf20982542740470b4d7ebc06223140fe7db
alishahi <shahy8227@gmail.com>
2024-01-21 13:16:06 -0800

    end code freelancer

--- src/Components/freelancer/Freelancer.jsx
+++ src/Components/freelancer/Freelancer.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function Freelancer() {
   return (
-    <div>Freelancer</div>
+    <div className="freelancer py-5 px-5 w-full bg-red-500 mb-5 dark:bg-gray-700">
+        <div className="container p-5">
+            <div className="freelancer-title p-5 text-center">
+                <p className="text-white text-lg mb-3">آیا شما پروژه ای دارید؟</p>
+                <p className="text-white text-3xl mb-5">من در دسترس هستم</p>
+                <a href="#" className='border py-2 text-lg rounded px-4 text-white mt-3 transition-all hover:bg-white hover:text-black'>کارتو به من بسپار</a>
+            </div>
+        </div>
+    </div>
   )
 }
 
