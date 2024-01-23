fde56a5de398f321f79f8fb6bd84125a49f0c485
alishahi <shahy8227@gmail.com>
2024-01-23 02:53:55 -0800

    create a button for new captcha number

--- src/Components/cantact/Contact.jsx
+++ src/Components/cantact/Contact.jsx
@@ -79,7 +79,7 @@ function Contact() {
             });
           }
         });
-    }else{
+    } else {
       Swal.fire({
         icon: "warning",
         title: "خطا",
@@ -231,6 +231,23 @@ function Contact() {
                     برای ارسال پیام حاصل جمع دو عدد را در کادر مشخص شده وارد
                     کنید
                   </p>
+                  <div className="newcaptcha my-auto mx-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="w-5 h-5 cursor-pointer"
+                      onClick={generateCaptcha}
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                      />
+                    </svg>
+                  </div>
                   <span className="mx-2 text-2xl">{randomNumber1}</span>
                   <span className="mx-2 text-2xl">+</span>
                   <span className="mx-2 text-2xl">{randomNumber2}</span>
